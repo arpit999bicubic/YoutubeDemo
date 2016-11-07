44345179192ff7994912827dccd793471be70cad
arpit999bicubic <androi.bicubics@gmail.com>
2016-11-07 17:58:12 +0530

    Initial commit

---
+++ app/src/main/java/com/bicubic/youtubedemo/Config.java
@@ -0,0 +1,13 @@
+package com.bicubic.youtubedemo;
+
+/**
+ * Created by echessa on 7/17/15.
+ */
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "A7:D4:30:57:5F:25:8C:90:C1:D6:C5:9C:25:14:7D:C4:A3:00:B4:20";
+
+}
